7372303cda212f81914160dc7a8fa4b826af445e
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-06-07 21:16:29 +0600

    make api for get top rated fetured rooms

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const { getDB } = require("../Config/db")
 
 exports.startServer = (req, res) => {
@@ -20,7 +21,7 @@ exports.getAllReviews = async (req, res) => {
         const reviewCollection = db.collection('roomsReviews');
         const result = await reviewCollection
             .find()
-            .sort({ date: -1 })
+            .sort({ _id: -1 })
             .toArray();
 
         res.status(200).json(result);
@@ -29,3 +30,27 @@ exports.getAllReviews = async (req, res) => {
         res.status(500).json({ error: "Failed to fetch review data." });
     }
 };
+
+exports.getFeaturedRooms = async (req, res) => {
+    try {
+        const db = getDB();
+        const reviewCollection = db.collection("roomsReviews");
+        const roomsCollection = db.collection("hotels");
+
+        const reviews = await reviewCollection.find().sort({ rating: -1 }).limit(6).toArray();
+
+        const roomIds = [...new Set(reviews.map(review => review.roomId))];
+
+        const result = await roomsCollection.find({
+            id: { $in: roomIds.map(id => id) }
+        }).toArray();
+        res.send(result);
+
+    } catch (error) {
+        console.error("Error fetching featured rooms:", error);
+        res.status(500).json({ error: "Failed to fetch featured rooms." });
+    }
+};
+
+
+
--- Routes/routes.js
+++ Routes/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { startServer, getAllHotel, getAllReviews } = require('../Controller/controller');
+const { startServer, getAllHotel, getAllReviews, getFeaturedRooms } = require('../Controller/controller');
 const routes = express.Router();
 
 routes.get('/', startServer)
 routes.get('/all/hotels', getAllHotel)
 routes.get('/all/reviews', getAllReviews)
+routes.get('/all/featured', getFeaturedRooms)
 module.exports = routes;
